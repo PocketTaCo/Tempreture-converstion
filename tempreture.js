6729b3e3e2049f0e3e9e67a8686ceaf47455fa0a
PocketTaCo <wizardrobe8@gmail.com>
2017-11-15 14:11:51 -1000

    hi

--- tempreture.js
+++ tempreture.js
@@ -1,4 +1,24 @@
 function TempretureConvertion(F){
-	return (F-32) / 1.8;
+	return "Fahrenheit is " + F + " and Celsius is" + (F-32) / 1.8; 
 }
 console.log(TempretureConvertion(20)); 
+console.log(TempretureConvertion(25));
+console.log(TempretureConvertion(30));
+console.log(TempretureConvertion(35));
+console.log(TempretureConvertion(40));
+console.log(TempretureConvertion(45));
+console.log(TempretureConvertion(50));
+console.log(TempretureConvertion(55));
+console.log(TempretureConvertion(60));
+console.log(TempretureConvertion(65));
+console.log(TempretureConvertion(70));
+console.log(TempretureConvertion(75));
+console.log(TempretureConvertion(80));
+console.log(TempretureConvertion(85));
+console.log(TempretureConvertion(90));
+console.log(TempretureConvertion(95));
+console.log(TempretureConvertion(100));
+console.log(TempretureConvertion(105));
+console.log(TempretureConvertion(110));
+console.log(TempretureConvertion(115));
+console.log(TempretureConvertion(120));
